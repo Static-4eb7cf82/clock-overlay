b31ee611901582fa4e8d80ac67f05c6a8784f8b2
static_4eb7cf82 <static_4eb7cf82@gmail.com>
2026-02-15 10:28:10 -0800

    more devcontainer setup/config

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,12 +2,20 @@
 set -e
 
 
-echo "cargo version: $(cargo --version)"
-echo "node version: $(node -v)"
-echo "npm version: $(npm -v)"
+echo "Installed versions:"
+echo "  rustc : $(rustc --version)"
+echo "  cargo : $(cargo --version)"
+echo "  node  : $(node -v)"
+echo "  npm   : $(npm -v)"
+echo ""
 
 echo "Installing npm dependencies..."
 cd clock-overlay
 npm install
 
-echo "Setup complete! Run 'npm run tauri dev' to start development."
+yellow="\033[1;33m"
+reset="\033[0m"
+echo -e "${yellow}The devcontainer timezone is currently set to $(cat /etc/timezone).${reset}"
+echo "To set the timezone for the devcontainer based on your local timezone, run the following command: sudo dpkg-reconfigure tzdata"
+
+echo "Run 'npm run tauri dev' to start development."
